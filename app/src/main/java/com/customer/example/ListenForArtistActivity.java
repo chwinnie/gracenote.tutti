79794639fb903fcff4392d96ba5649d15c0df984
chwinnie <winnie@vouch.com>
2015-07-12 03:20:01 -0700

    added MusicIDStreamEvents.. but not really doing cancellations yet

--- app/src/main/java/com/customer/example/ListenForArtistActivity.java
+++ app/src/main/java/com/customer/example/ListenForArtistActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,7 @@ import com.gracenote.gnsdk.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 
 public class ListenForArtistActivity extends Activity {
@@ -27,6 +29,20 @@ public class ListenForArtistActivity extends Activity {
 
     // Gracenote objects
     private GnManager 					gnManager;
+    private GnUser 						gnUser;
+    private GnMusicIdStream 			gnMusicIdStream;
+    private IGnAudioSource				gnMicrophone;
+
+    // store some tracking info about the most recent MusicID-Stream lookup
+    protected volatile boolean 			lastLookup_local		 = false;	// indicates whether the match came from local storage
+    protected volatile long				lastLookup_matchTime 	 = 0;  		// total lookup time for query
+    protected volatile long				lastLookup_startTime;  				// start time of query
+    private volatile boolean			audioProcessingStarted   = false;
+    private volatile boolean			analyzingCollection 	 = false;
+    private volatile boolean			analyzeCancelled 	 	 = false;
+
+    // UI Objects
+    Button listen_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +52,29 @@ public class ListenForArtistActivity extends Activity {
         activity = this;
         context  = this.getApplicationContext();
 
+        createUI();
+
         // get the gnsdk license from the application assets
         String gnsdkLicense = getAssetAsString(gnsdkLicenseFilename);
 
         try {
             gnManager = new GnManager( context, gnsdkLicense, GnLicenseInputMode.kLicenseInputModeString );
+            gnUser = new GnUser( new GnUserStore(context), gnsdkClientId, gnsdkClientTag, appString );
+
+            // enable local MusicID-Stream recognition (GNSDK storage provider must be enabled as pre-requisite)
+            GnLookupLocalStream.enable();
+
+            // Ingest MusicID-Stream local bundle, perform in another thread as it can be lengthy
+            Thread ingestThread = new Thread( new LocalBundleIngestRunnable(context) );
+            ingestThread.start();
+
+            gnMicrophone = new GnMic();
+            gnMusicIdStream = new GnMusicIdStream( gnUser, GnMusicIdStreamPreset.kPresetMicrophone, new MusicIDStreamEvents() );
+            gnMusicIdStream.options().lookupData(GnLookupData.kLookupDataContent, true);
+            gnMusicIdStream.options().lookupData(GnLookupData.kLookupDataSonicData, true);
+            gnMusicIdStream.options().resultSingle(true);
+
+
         } catch ( GnException e ) {
             Log.e(appString, e.errorCode() + ", " + e.errorDescription() + ", " + e.errorModule() );
             return;
@@ -56,6 +90,10 @@ public class ListenForArtistActivity extends Activity {
         }
     }
 
+    public void createUI() {
+        listen_button = (Button) findViewById(R.id.listen_button);
+        listen_button.setEnabled( false );
+    }
 
     public void initializeGraceNote(View v) {
         Button btn = (Button) findViewById(R.id.listen_button);
@@ -94,7 +132,16 @@ public class ListenForArtistActivity extends Activity {
     }
 
 
+    /**
+     * GNSDK bundle ingest status event delegate
+     */
+    private class BundleIngestEvents implements IGnLookupLocalStreamIngestEvents{
 
+        @Override
+        public void statusEvent(GnLookupLocalStreamIngestStatus status, String bundleId, IGnCancellable canceller) {
+//            setStatus("Bundle ingest progress: " + status.toString() , true);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -116,5 +163,131 @@ public class ListenForArtistActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Loads a local bundle for MusicID-Stream lookups
+     */
+    class LocalBundleIngestRunnable implements Runnable {
+        Context context;
+
+        LocalBundleIngestRunnable(Context context) {
+            this.context = context;
+        }
+
+        public void run() {
+            try {
+
+                // our bundle is delivered as a package asset
+                // to ingest the bundle access it as a stream and write the bytes to
+                // the bundle ingester
+                // bundles should not be delivered with the package as this, rather they
+                // should be downloaded from your own online service
+
+                InputStream 	bundleInputStream 	= null;
+                int				ingestBufferSize	= 1024;
+                byte[] 			ingestBuffer 		= new byte[ingestBufferSize];
+                int				bytesRead			= 0;
+
+                GnLookupLocalStreamIngest ingester = new GnLookupLocalStreamIngest(new BundleIngestEvents());
+
+                try {
+
+                    bundleInputStream = context.getAssets().open("1557.b");
+
+                    do {
+
+                        bytesRead = bundleInputStream.read(ingestBuffer, 0, ingestBufferSize);
+                        if ( bytesRead == -1 )
+                            bytesRead = 0;
+
+                        ingester.write( ingestBuffer, bytesRead );
+
+                    } while( bytesRead != 0 );
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                ingester.flush();
+
+            } catch (GnException e) {
+                Log.e( appString, e.errorCode() + ", " + e.errorDescription() + ", " + e.errorModule() );
+            }
+
+        }
+    }
+
+    /**
+     * GNSDK MusicID-Stream event delegate
+     */
+    private class MusicIDStreamEvents implements IGnMusicIdStreamEvents {
+
+        HashMap<String, String> gnStatus_to_displayStatus;
+
+        public MusicIDStreamEvents(){
+            gnStatus_to_displayStatus = new HashMap<String,String>();
+            gnStatus_to_displayStatus.put(GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingStarted.toString(), "Identification started");
+            gnStatus_to_displayStatus.put(GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingFpGenerated.toString(), "Fingerprinting complete");
+            gnStatus_to_displayStatus.put(GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingLocalQueryStarted.toString(), "Lookup started");
+            gnStatus_to_displayStatus.put(GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingOnlineQueryStarted.toString(), "Lookup started");
+//			gnStatus_to_displayStatus.put(GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingEnded.toString(), "Identification complete");
+        }
+
+        @Override
+        public void statusEvent( GnStatus status, long percentComplete, long bytesTotalSent, long bytesTotalReceived, IGnCancellable cancellable ) {
+
+        }
+
+        @Override
+        public void musicIdStreamProcessingStatusEvent( GnMusicIdStreamProcessingStatus status, IGnCancellable canceller ) {
+
+            if(GnMusicIdStreamProcessingStatus.kStatusProcessingAudioStarted.compareTo(status) == 0)
+            {
+                audioProcessingStarted = true;
+                activity.runOnUiThread(new Runnable (){
+                    public void run(){
+                        listen_button.setEnabled(true);
+                    }
+                });
+
+            }
+
+        }
+
+        @Override
+        public void musicIdStreamIdentifyingStatusEvent( GnMusicIdStreamIdentifyingStatus status, IGnCancellable canceller ) {
+            if(gnStatus_to_displayStatus.containsKey(status.toString())){
+//                setStatus( String.format("%s", gnStatus_to_displayStatus.get(status.toString())), true );
+            }
+
+            if(status.compareTo( GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingLocalQueryStarted ) == 0 ){
+                lastLookup_local = true;
+            }
+            else if(status.compareTo( GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingOnlineQueryStarted ) == 0){
+                lastLookup_local = false;
+            }
+
+            if ( status == GnMusicIdStreamIdentifyingStatus.kStatusIdentifyingEnded )
+            {
+//                setUIState( UIState.READY );
+            }
+        }
+
+
+        @Override
+        public void musicIdStreamAlbumResult( GnResponseAlbums result, IGnCancellable canceller ) {
+            lastLookup_matchTime = SystemClock.elapsedRealtime() - lastLookup_startTime;
+//            activity.runOnUiThread(new UpdateResultsRunnable( result ));
+        }
+
+        @Override
+        public void musicIdStreamIdentifyCompletedWithError(GnError error) {
+//            if ( error.isCancelled() )
+//                setStatus( "Cancelled", true );
+//            else
+//                setStatus( error.errorDescription(), true );
+//                setUIState( UIState.READY );
+        }
     }
 }
