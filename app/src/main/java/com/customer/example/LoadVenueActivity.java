a8205cdb2eb3f5077293fb319a737e97c500a562
chwinnie <winnie@vouch.com>
2015-07-12 01:12:17 -0700

    grace note config and old code

---
+++ app/src/main/java/com/customer/example/LoadVenueActivity.java
@@ -0,0 +1,46 @@
+package com.customer.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class LoadVenueActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_venue);
+
+        Bundle bundle = getIntent().getExtras();
+        String message = bundle.getString("audioURL");
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(message);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_load_venue, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
